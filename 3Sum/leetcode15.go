68f8d8e6f837f52dfe051102ac877b9822d01d2d
Cristian Gutiérrez <ggcristian@icloud.com>
2022-09-30 22:23:43 +0200

    3Sum

--- 3Sum/leetcode15.go
+++ 3Sum/leetcode15.go
@@ -1,20 +1,58 @@
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+
 package leetcode15
 
-func twoSum(numbers []int, target int) []int {
-	if len(numbers) <= 2 {
-		return []int{1, 2}
-	}
-	ptr_in := 0
-	ptr_fi := len(numbers) - 1
-	for ptr_in < len(numbers) && ptr_fi >= 0 {
-		if numbers[ptr_in]+numbers[ptr_fi] > target {
-			ptr_fi -= 1
-		} else if numbers[ptr_in]+numbers[ptr_fi] < target {
-			ptr_in += 1
-		}
-		if numbers[ptr_in]+numbers[ptr_fi] == target {
-			return []int{ptr_in + 1, ptr_fi + 1}
+func checkMap(val_i, val_j, val_k int, m map[int][]int) (res bool) {
+	for _, vals_i := range m[val_i] {
+		for _, vals_j := range m[val_j] {
+			for _, vals_k := range m[val_k] {
+				if vals_j == vals_k && vals_i == vals_j {
+					return true
+				}
+			}
 		}
 	}
-	return []int{}
+	return false
+}
+
+func appendMap(val_i, val_j, val_k int, m map[int][]int, global_ctr int) map[int][]int {
+	m[val_i] = append(m[val_i], global_ctr)
+	m[val_j] = append(m[val_j], global_ctr)
+	m[val_k] = append(m[val_k], global_ctr)
+	return m
+}
+
+// O(N^3)
+// Brute force, three pointers and checking each pointer
+func threeSum(nums []int) [][]int {
+	res := [][]int{}
+	if len(nums) == 1 && nums[0] == 0 {
+		return append(res, []int{0, 0, 0})
+	}
+
+	m := make(map[int][]int)
+	global_ctr := 0
+	allZeros := false
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			for k := j + 1; k < len(nums); k++ {
+				if !allZeros && (nums[i] == 0 && nums[j] == 0 && nums[k] == 0) {
+					m = appendMap(nums[i], nums[j], nums[k], m, global_ctr)
+					global_ctr += 1
+					res = append(res, []int{nums[i], nums[j], nums[k]})
+					allZeros = true
+				} else if nums[i]+nums[j]+nums[k] == 0 && !checkMap(nums[i], nums[j], nums[k], m) {
+					m = appendMap(nums[i], nums[j], nums[k], m, global_ctr)
+					global_ctr += 1
+					res = append(res, []int{nums[i], nums[j], nums[k]})
+				}
+			}
+		}
+	}
+	return res
 }
--- 3Sum/leetcode15_test.go
+++ 3Sum/leetcode15_test.go
@@ -6,28 +6,28 @@ import (
 )
 
 func TestTwoSum(t *testing.T) {
-	assertSlice := func(t testing.TB, got, want []int) {
+	assertSlice := func(t testing.TB, got, want [][]int) {
 		t.Helper()
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
 	}
 
-	t.Run(" (1)", func(t *testing.T) {
-		got := twoSum([]int{2, 7, 11, 15}, 9)
-		want := []int{1, 2}
+	t.Run("test (1)", func(t *testing.T) {
+		got := threeSum([]int{-1, 0, 1, 2, -1, -4})
+		want := [][]int{{-1, -1, 2}, {-1, 0, 1}}
 		assertSlice(t, got, want)
 	})
 
-	t.Run(" (2)", func(t *testing.T) {
-		got := twoSum([]int{2, 3, 4}, 6)
-		want := []int{1, 3}
+	t.Run("test (2)", func(t *testing.T) {
+		got := threeSum([]int{0, 1, 1})
+		want := [][]int{}
 		assertSlice(t, got, want)
 	})
 
-	t.Run(" (3)", func(t *testing.T) {
-		got := twoSum([]int{-1, 0}, -1)
-		want := []int{1, 2}
+	t.Run("test (3)", func(t *testing.T) {
+		got := threeSum([]int{0, 0, 0})
+		want := [][]int{{0, 0, 0}}
 		assertSlice(t, got, want)
 	})
 }
